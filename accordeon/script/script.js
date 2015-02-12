1de3516fd916011d0dbd64193d9bea903d48b7e7
Anton <antonkopilec@gmail.com>
2015-02-12 14:33:36 +0300

    navigations

---
+++ accordeon/script/script.js
@@ -0,0 +1,80 @@
+var createAccordeon = function(jsonObject){
+	var accordeon = document.getElementById("accordeon");
+	accordeon.innerHTML = "<ul class='accordeon-level'></ul>";
+	parseJSON(jsonObject);
+}
+
+
+var parseJSON = function(jsonObject){
+	var accordeonData;
+	try{
+		accordeonData = JSON.parse(jsonObject);
+	}
+	catch(e){
+		console.log(e.message);
+		console.log("invalid JSON");
+		return;
+	}
+	console.log(accordeonData);
+	var accordeon = document.getElementById("accordeon");
+	createAccordeonLevel(accordeon,accordeonData);
+};
+
+var createAccordeonLevel = function(nodeToInsert,accordeonData){
+	if(accordeonData !== null){
+
+		for(var accordTitle in accordeonData){
+			if(accordeonData.hasOwnProperty(accordTitle)){
+				var isObject = (typeof(accordeonData[accordTitle]) === "object" && accordeonData[accordTitle] !== null && !Array.isArray(accordeonData[accordTitle]));
+				if(isObject){
+				}
+				else{
+					createInset(accordTitle,accordeonData[accordTitle]);
+				}
+			} //if
+		} //for
+	} //if
+};
+
+var createInset = function(insetTitle,insetData){
+	var accordLevel = document.getElementsByClassName("accordeonLevel")[0];
+				
+	var li = document.createElement('li');
+	accordLevel.appendChild(li);
+
+	var accordeonItem = document.createElement('div');
+	accordeonItem.classList.add("accordeonItem");
+	li.appendChild(accordeonItem);
+
+	// creation of title
+	var a = document.createElement("a");
+	a.setAttribute("href","#");
+	a.addEventListener('click',toogleText);
+	a.appendChild(document.createTextNode(insetTitle));
+	accordeonItem.appendChild(a);
+
+	//creation of textdata
+	var p;
+	if(Array.isArray(insetData)){
+		for(var i = 0; i < insetData.length; i++){
+			p = document.createElement("p");
+			p.classList.add("hide");
+			p.appendChild(document.createTextNode(insetData[i]));
+			accordeonItem.appendChild(p);
+		}
+	}
+	else{
+		p = document.createElement("p");
+		p.classList.add("hide");
+		p.appendChild(document.createTextNode(insetData));
+		accordeonItem.appendChild(p);
+	}
+}
+var toogleText = function(e){
+	var next = this.nextSibling;
+	while(next){
+		next.classList.toggle("hide");
+		next = next.nextSibling;
+	}
+}
+createAccordeon(JSON.stringify({Example1:["John","is","cool"],Example2:"42"}));
---
+++ treeaccordeon/script/script.js
@@ -0,0 +1,86 @@
+var parseJSON = function(jsonObject){
+	var accordeonData;
+	try{
+		accordeonData = JSON.parse(jsonObject);
+	}
+	catch(e){
+		console.log(e.message);
+		console.log("invalid JSON");
+		return;
+	}
+	console.log(accordeonData);
+	var accordeon = document.getElementById("accordeon");
+	var mustHide = false;
+	createAccordeonLevel(accordeon,accordeonData,mustHide);
+};
+
+var createAccordeonLevel = function(nodeToInsert,accordeonData,mustHide){
+	if(accordeonData !== null){
+		var accordLevel = document.createElement("ul");
+		accordLevel.classList.add("accordeonLevel");
+		if(mustHide){
+			accordLevel.classList.add("hide");
+			accordLevel.classList.add("content");
+		}
+		nodeToInsert.appendChild(accordLevel);
+		for(var accordTitle in accordeonData){
+			if(accordeonData.hasOwnProperty(accordTitle)){
+				createAccordeonItem(accordLevel,accordTitle,accordeonData[accordTitle]);
+			} //if
+		} //for
+	} //if
+};
+var createAccordeonItem = function(nodeToInsert,insetTitle,insetData){
+	var isObject = (typeof(insetData) === "object" && insetData !== null && !Array.isArray(insetData));
+	var li = document.createElement('li');
+	nodeToInsert.appendChild(li);
+
+	var accordeonItem = document.createElement('div');
+	accordeonItem.classList.add("accordeonItem");
+	li.appendChild(accordeonItem);
+
+	createTitle(accordeonItem,insetTitle);
+	var mustHide = true;
+	if(isObject){
+		createAccordeonLevel(accordeonItem,insetData,mustHide)
+	}
+	else{
+		createInset(accordeonItem,insetData);
+	}
+}
+var createTitle = function(nodeToInsert,insetTitle){
+	// creation of title
+	var a = document.createElement("a");
+	a.setAttribute("href","#");
+	a.addEventListener('click',toogleText);
+	a.appendChild(document.createTextNode(insetTitle));
+	nodeToInsert.appendChild(a);
+}
+var createInset = function(nodeToInsert,insetData){
+	//creation of textdata
+	var p;
+	if(Array.isArray(insetData)){
+		for(var i = 0; i < insetData.length; i++){
+			p = document.createElement("p");
+			p.classList.add("hide");
+			p.classList.add("content");
+			p.appendChild(document.createTextNode(insetData[i]));
+			nodeToInsert.appendChild(p);
+		}
+	}
+	else{
+		p = document.createElement("p");
+		p.classList.add("hide");
+		p.classList.add("content");
+		p.appendChild(document.createTextNode(insetData));
+		nodeToInsert.appendChild(p);
+	}
+}
+var toogleText = function(e){
+	var next = this.nextSibling;
+	while(next){
+		next.classList.toggle("hide");
+		next = next.nextSibling;
+	}
+}
+parseJSON(JSON.stringify({Example1:["John","is","cool"],Example2:"42",Example3:{subExample:"good"}}));
